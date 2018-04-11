98a0e607240d9a9bc6f2845f4fba6e9b1fe0e841
tuno12 <artur.dudkiewicz@gmail.com>
2018-04-11 12:38:29 +0300

    new

--- inputs.js
+++ inputs.js
@@ -1,10 +1,24 @@
-class NameField extends React.Component {
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: "",
+      last: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
   render() {
-    return (<div><input onInput={this.value}></input></div>);
+    return <div>
+      <label>
+        First
+      <input type="text" name="last" oninput="{" this.handlechange="" }="" </label <label Last
+        <input type="text" <hr <output {this.state.last}/> > > /> > /> >{this.state.first} </output>
+    </div>;
   }
 }
-
-function App() {
-  return (<NameField />);
-}
-ReactDOM.render(<App/>, document.getElementById("app"));
